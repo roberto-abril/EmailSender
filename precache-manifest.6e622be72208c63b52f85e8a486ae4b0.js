f2c4bc64f17df6216074b4b5f389000c2504dd75
Roberto <>
2020-07-20 15:38:32 +0200

    Version 1

---
+++ precache-manifest.6e622be72208c63b52f85e8a486ae4b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11f3f64deed9bbca5310f1a65ab5561",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5de40071389a216ca8a",
+    "url": "/static/css/main.4084ad9a.chunk.css"
+  },
+  {
+    "revision": "ec2d8531568369270bf3",
+    "url": "/static/js/2.4ef7198f.chunk.js"
+  },
+  {
+    "revision": "1bcefd02b7d05499a818042383aa8e83",
+    "url": "/static/js/2.4ef7198f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5de40071389a216ca8a",
+    "url": "/static/js/main.05e62cdd.chunk.js"
+  },
+  {
+    "revision": "170794694db56f00f0d4",
+    "url": "/static/js/runtime-main.0a08b959.js"
+  }
+]);
